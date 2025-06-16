0091c1b1b8c47c159538f0ee88351773d8ce290d
daianebluchinski <daiane.bluchinski@escola.pr.gov.br>
2025-06-16 09:17:23 -0300

    ok

--- script.js
+++ script.js
@@ -12,9 +12,10 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
-    const frequencias = contaFrequencias(palavras);
-    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
-
+    for (let i in palavras) {
+    palavras[i] = palavras[i].toLowerCase();
+}
+palavras = tirapalavrasruins (palavras)
     function ordenaPalavra(p1, p2) {
         return frequencias[p2] - frequencias[p1];
     }
@@ -35,3 +36,139 @@ function contaFrequencias(palavras) {
     }
     return frequencias;
 }
+
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set([
+    "que",
+    "para",
+    "com",
+    "não",
+    "uma",
+    "por",
+    "mais",
+    "dos",
+    "como",
+    "mas",
+    "foi",
+    "ele",
+    "das",
+    "tem",
+    "seu",
+    "sua",
+    "ser",
+    "quando",
+    "muito",
+    "está",
+    "também",
+    "pelo",
+    "pela",
+    "até",
+    "isso",
+    "ela",
+    "entre",
+    "era",
+    "depois",
+    "sem",
+    "mesmo",
+    "aos",
+    "ter",
+    "seus",
+    "quem",
+    "nas",
+    "esse",
+    "eles",
+    "estão",
+    "você",
+    "tinha",
+    "foram",
+    "essa",
+    "num",
+    "nem",
+    "suas",
+    "meu",
+    "minha",
+    "têm",
+    "numa",
+    "pelos",
+    "elas",
+    "havia",
+    "seja",
+    "qual",
+    "será",
+    "nós",
+    "tenho",
+    "lhe",
+    "deles",
+    "essas",
+    "esses",
+    "pelas",
+    "este",
+    "fosse",
+    "dele",
+    "vocês",
+    "vos",
+    "lhes",
+    "meus",
+    "minhas",
+    "teu",
+    "tua",
+    "teus",
+    "tuas",
+    "nosso",
+    "nossa",
+    "nossos",
+    "nossas",
+    "dela",
+    "delas",
+    "esta",
+    "estes",
+    "estas",
+    "aquele",
+    "aquela",
+    "aqueles",
+    "aquelas",
+    "isto",
+    "aquilo",
+    "estou",
+    "estamos",
+    "estive",
+    "esteve",
+    "estivemos",
+    "estiveram",
+    "estava",
+    "estávamos",
+    "estavam",
+    "e",
+    "ou",
+    "onde",
+    "aquilo",
+    "embora",
+    "apesar",
+    "porque",
+    "enquanto",
+    "contudo",
+    "entretanto",
+    "portanto",
+    "além",
+    "antes",
+    "todavia",
+    "inclusive",
+    "diante",
+    "sobre",
+    "quanto",
+    "apenas",
+    "desde",
+    "uma vez",
+    "segundo",
+    "junto"
+]);
+    const palavras_ruins =new set ["para, uma, nós"]
+    const palavrasBoas =  [];
+    for (let palavra of palavras) {
+    if (palavras_ruins.has (palavras) .length > 2) {
+        palavrasBoas.push(palavra);
+    }
+}
+return palavrasBoas;
+}
+
